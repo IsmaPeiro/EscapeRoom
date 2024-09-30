ea788b6ddf926d2001312a9bb349adc72eed2488
Ramon <ponti1990@hotmail.com>
2024-09-30 11:38:52 +0200

    Cambios en Clue

--- src/main/java/model/escape_room/ClueUtils.java
+++ src/main/java/model/escape_room/ClueUtils.java
@@ -2,23 +2,54 @@ package model.escape_room;
 
 import model.clues.Clue;
 import model.rooms.Room;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class ClueUtils {
-    public static void addClue (Room room) {
-        room.createClue();
+    public static Clue addClue (Room room) {
+        Clue clue = null;
+        Connection conn = null;
+        List<Clue> clues = null;
+        try{
+            conn = MySQLUtils.getConn();
+            ClueDAO dao = new MySQLClueDAO(conn);
+            clues = dao.readAvailable(room.getThematic());
+        }catch (DAOException | SQLException e){
+            System.out.println(e);
+        }finally {
+            MySQLUtils.closeConn(conn);
+        }
+        if (!clues.isEmpty()){
+            clues.forEach(System.out::println);
+            while (clue == null){
+                int clueID = Input.readInt("Insert the id of the clue:");
+                clue = clues.stream().filter(c -> c.getId == clueID).findFirst().orElse(null);
+                if(clue == null) System.out.println("Invalid clue");
+            }
+        }else{
+            System.out.println("No clues available.");
+        }
+        return clue;
     }
 
     public static void removeClue (Room room){
-//        int id = room.getId();
-//
-//        Clue clue = room.findClue(id);
-//        if (clue != null){
-//            room.removeClueFromList(clue);
-//            System.out.println("Clue removed.");
-//        }else{
-//            System.out.println("Nonexistent clue.");
-//        }
+        Clue clue = null;
+        Connection conn = null;
+        List<Clue> clues = room.getClues();
+
+        if(!clues.isEmpty()){
+            clues.forEach(System.out::println);
+            while (clue == null) {
+                int clueID = Input.readInt("Insert the id of the clue:");
+                clue = clues.stream().filter(c -> c.getId() == clueID).findFirst().orElse(null);
+                if (clue == null) System.out.println("Invalid clue.");
+            }
+        }else{
+            System.out.println("No clues available.");
+        }
+        return clue;
     }
 
     public static float getClueScore(){
